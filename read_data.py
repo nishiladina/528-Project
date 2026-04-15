324782b4302f5d7ee6165ebd50f069a345215c58
David Makarovsky <62029977+davidmakarovsky@users.noreply.github.com>
2026-04-15 15:59:49 -0400

    Uploaded read_data
    Used for reading data from ESP and saving to folder

---
+++ read_data.py
@@ -0,0 +1,47 @@
+import os
+import serial
+
+PORT = "COM3"       # port number
+BAUD = 115200       
+OUTPUT_DIR = "../up" #change folder name to the gesture being recorded
+
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+ser = serial.Serial(PORT, BAUD, timeout=1)
+
+current_file = None
+
+try:
+    while True:
+        line = ser.readline().decode("utf-8", errors="replace").strip()
+        if not line:
+            continue
+
+        print(line)
+
+        if line.startswith("FILE_START:"):
+            num = int(line.split(":")[1])
+            
+            # Change up to the gesture being recorded
+            filename = os.path.join(OUTPUT_DIR, f"up_{num:02d}.txt")
+            current_file = open(filename, "w")
+            continue
+
+        if line.startswith("FILE_END:"):
+            print("\n\n\n\n\n")
+            if current_file is not None:
+                current_file.close()
+                current_file = None
+
+            num = int(line.split(":")[1])
+            if num == 33:
+                break
+            continue
+
+        if current_file is not None and line:
+            current_file.write(line + "\n")
+
+finally:
+    if current_file is not None:
+        current_file.close()
+    ser.close()
